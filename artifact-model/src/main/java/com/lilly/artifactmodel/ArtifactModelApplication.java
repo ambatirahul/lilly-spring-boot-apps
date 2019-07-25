b2d6757617ee6ee9c043c7cf517b6f3b5cd7bf05
Rahul Ambati <ambatirahulreddy@gmail.com>
2019-07-25 01:42:43 -0500

    Added indentation and java-doc.

--- artifact-model/src/main/java/com/lilly/artifactmodel/ArtifactModelApplication.java
+++ artifact-model/src/main/java/com/lilly/artifactmodel/ArtifactModelApplication.java
@@ -3,11 +3,14 @@ package com.lilly.artifactmodel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A mock spring-boot app
+ *
+ * @author Rahul
+ */
 @SpringBootApplication
 public class ArtifactModelApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ArtifactModelApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ArtifactModelApplication.class, args);
+    }
 }
