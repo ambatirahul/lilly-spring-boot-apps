324574390349b91d92cf86bde5e22b5dacbaad6f
Rahul Ambati <ambatirahulreddy@gmail.com>
2019-07-25 01:15:22 -0500

    Adding a new spring-boot artifact project.

---
+++ artifact-model/src/main/java/com/lilly/artifactmodel/ArtifactModelApplication.java
@@ -0,0 +1,13 @@
+package com.lilly.artifactmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactModelApplication.class, args);
+	}
+
+}
